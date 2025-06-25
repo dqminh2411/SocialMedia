7405342ec58cc8a8ea06050bc6b37fd7e8108597
Doan Minh <doanminhyb04@gmail.com>
2025-06-26 00:28:16 +0700

    add swagger openAPI doc UI

---
+++ backend/src/main/java/com/ttcs/socialmedia/config/OpenAPIConfig.java
@@ -0,0 +1,41 @@
+package com.ttcs.socialmedia.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+    private String devUrl = "http://localhost:8080/";
+
+    private SecurityScheme createAPIKeyScheme(){
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("Bearer");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Development Server URL");
+
+        Contact contact = new Contact();
+        contact.setEmail("doanminhyb04@gmail.com");
+
+        License license = new License().name("MIT License").url("https://opensource.org/licenses/MIT");
+
+        Info info = new Info().contact(contact).license(license).title("Social Media API Doc").version("1.0").description("Social Media API Doc");
+
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication")).components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme())).info(info).servers(List.of(devServer));
+    }
+
+
+}
--- backend/src/main/java/com/ttcs/socialmedia/config/SecurityConfiguration.java
+++ backend/src/main/java/com/ttcs/socialmedia/config/SecurityConfiguration.java
@@ -2,7 +2,11 @@ package com.ttcs.socialmedia.config;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import com.nimbusds.jose.util.Base64;
+import com.ttcs.socialmedia.repository.UserRepository;
+//import com.ttcs.socialmedia.service.CustomOauth2UserService;
+import com.ttcs.socialmedia.service.UserService;
 import com.ttcs.socialmedia.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +17,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -20,28 +25,37 @@ import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfiguration {
-    @Value("${app.jwt.base64-secret}")
-    private String jwtKey;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+
+//    @Bean
+//    public OAuth2UserService customOAuth2UserService(UserRepository userRepository, UserService userService){
+//        return new CustomOauth2UserService(userRepository,userService);
+//    }
+
+
+
+
+    String[] whiteLists = {"/", "/auth/login", "/auth/social-login", "/auth/social/callback", "/users/signup", "/auth/refresh", "/storage/**",
+            "/ws", "/actuator/**", "/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**"};
     @Bean
     // , CustomAuthenticationEntryPoint customAuthenticationEntryPoint
     public SecurityFilterChain filterChain(HttpSecurity http,
-            CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
+                                           CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults()).authorizeHttpRequests(
-                authz -> authz.requestMatchers("/", "/auth/login", "/users/signup", "/auth/refresh", "/storage/**",
-                        "/ws", "/actuator/**").permitAll().anyRequest().authenticated())
+                authz -> authz.requestMatchers(whiteLists).permitAll().anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
                 // .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(new
@@ -57,45 +71,6 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-    @Bean
-    public JwtEncoder jwtEncoder() {
-        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
-    }
 
-    private SecretKey getSecretKey() {
-        byte[] keyBytes = Base64.from(jwtKey).decode();
-        return new SecretKeySpec(keyBytes, 0, keyBytes.length, SecurityUtil.JWT_ALGORITHM.getName());
-    }
-
-    //
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(
-                getSecretKey()).macAlgorithm(SecurityUtil.JWT_ALGORITHM).build();
-        // the token string is provided by BearerTokenAuthenticationFilter
-        // Functional interface lambda
-        return token -> {
-            try {
-                return jwtDecoder.decode(token); // decode a token string to a JWT object
-            } catch (Exception e) {
-                System.out.println(">>> JWT error: " + e.getMessage());
-                throw e;
-            }
-        };
-    }
-
-    @Bean
-    // get users' authorities and authentication inside JWT to check at API
-    // endpoints
-    // the information is stored in SecurityContextHolder
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        grantedAuthoritiesConverter.setAuthorityPrefix("");
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("user");
-
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
-        return jwtAuthenticationConverter;
-    }
 
 }
