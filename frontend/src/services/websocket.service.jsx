4ae6c596f9ab82a5ed29727088b39e83be802f4f
Minh <doanminhyb04@gmail.com>
2026-05-16 15:32:48 +0700

    fix: use wss for websocket

--- frontend/src/services/websocket.service.jsx
+++ frontend/src/services/websocket.service.jsx
@@ -6,7 +6,7 @@ import {Client} from '@stomp/stompjs';
 const USER_CHAT = '/user/chat';
 const USER_NOTI = '/user/noti';
 const CHAT_DESTINATION = '/app/chat';
-const WS_BACKEND_URL = 'ws'+import.meta.env.VITE_BE_API_BASE_URL.slice(5)+'/ws';
+const WS_BACKEND_URL = 'wss'+import.meta.env.VITE_BE_API_BASE_URL.slice(5)+'/ws';
 
 
 class WebSocketService {
