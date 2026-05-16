a0f364e04e734c492e75c0127a8883eb28a29ff5
Minh <doanminhyb04@gmail.com>
2026-05-16 15:38:04 +0700

    fix: be api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 let isLoggedOut = false;
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BE_API_BASE_URL || 'http://localhost:8080/api/v1',
+    baseURL: import.meta.env.VITE_BE_API_BASE_URL + '/api/v1' || 'http://localhost:8080/api/v1',
     withCredentials: true
 })
 
