60b4ab874421ca18b41b39b438279f728611c4ee
Minh <doanminhyb04@gmail.com>
2026-05-16 14:12:02 +0700

    fix: api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import.meta.env.BE_API_BASE_URL;
 let isLoggedOut = false;
 
 const api = axios.create({
-    baseURL: import.meta.env.BE_API_BASE_URL || 'http://localhost:8080/api/v1',
+    baseURL: BE_API_BASE_URL || 'http://localhost:8080/api/v1',
     withCredentials: true
 })
 
