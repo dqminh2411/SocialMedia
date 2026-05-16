7f638ac5b2799b3eff17e28f6a119263095b33c9
Minh <doanminhyb04@gmail.com>
2026-05-16 14:25:02 +0700

    fix: api.js for VITE env vars

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import.meta.env.BE_API_BASE_URL;
 let isLoggedOut = false;
 
 const api = axios.create({
-    baseURL: BE_API_BASE_URL || 'http://localhost:8080/api/v1',
+    baseURL: import.meta.env.VITE_BE_API_BASE_URL || 'http://localhost:8080/api/v1',
     withCredentials: true
 })
 
