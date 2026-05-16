556ab482d996e859195eed6da97a0ed8853b9d09
Minh <doanminhyb04@gmail.com>
2026-05-16 14:57:08 +0700

    fix: change allowed origins in CorsConfig

--- backend/src/main/java/com/ttcs/socialmedia/config/CorsConfig.java
+++ backend/src/main/java/com/ttcs/socialmedia/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // cho phép các URL nào có thể kết nối tới backend
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:4173", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://socialmedia-fe.onrender.com"));
 
         // các method nào đc kết nối
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
